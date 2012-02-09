267eb9be772d77f3047ea724df1a0910aeb61f34
Mac book Air <jchappypig@hotmail.com>
2012-02-10 10:42:41 +1100

    initialization.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoyalChinese::Application.config.secret_token = 'd5b581f9add8f6a566480ab8722f586731374b041e142f2d489cc193bf1db4f65ca002eed047df6c0aba65538078cf0af59505048d298617e04886eb699c88d4'
