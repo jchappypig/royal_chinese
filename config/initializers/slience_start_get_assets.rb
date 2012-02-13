dcb108a032c771ad3c2a9d3d06bf1a8769136da7
Mac book Air <jchappypig@hotmail.com>
2012-02-14 07:45:58 +1100

    added comment for file that silence 'Started GET' console output.

--- config/initializers/slience_start_get_assets.rb
+++ config/initializers/slience_start_get_assets.rb
@@ -1,3 +1,6 @@
+# This part of code is to get rid of the annoying print out on console.
+# eg. Started GET "/assets/bootstrap.js?body=1" for 127.0.0.1 at 2012-02-13 23:43:48 +1100
+
 Rails::Rack::Logger.class_eval do
   def call_with_quiet_assets(env)
     previous_level = Rails.logger.level
