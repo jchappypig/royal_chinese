2bfdf1f41217351b748f13f784bee6011f22228f
Mac book Air <jchappypig@hotmail.com>
2012-02-17 20:33:20 +1100

    show image in menu.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ActiveRecord::Base
   validates :category, presence: true
   validates :price, presence: true
 
-  has_attached_file :image
+  has_attached_file :image, default_url: "/images/default_menu_image.png"
   validates_attachment_size :image, less_than: 1.megabytes
-  validates_attachment_content_type :image, content_type: ['image/png', 'image/gif', 'image/jpg']
+  validates_attachment_content_type :image, content_type: /image\/.+/
 end
