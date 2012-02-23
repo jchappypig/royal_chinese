93c291b1817b87e519ed20296c6e9bc4f3b48f75
Mac book Air <jchappypig@hotmail.com>
2012-02-24 08:12:19 +1100

    Refine gmaps4rails map

--- app/models/address.rb
+++ app/models/address.rb
@@ -11,6 +11,10 @@ class Address < ActiveRecord::Base
   end
 
   def gmaps4rails_infowindow
-    name
+    infowindow =  "<h3>#{name}</h3>"
+    infowindow << "<img src='/images/ziqi.jpg' width='100px' align='left'>"
+    infowindow << "<h4>Open Hour:</h4>"
+    infowindow << "<p><dd><b>Monday to Sunday</b><dd></p>"
+    infowindow << "<dd>6:00pm - 10:00pm</dd>"
   end
 end
