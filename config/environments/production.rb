46f75b6c4fcd9367501a35f613c14d111a2666ff
Mac book Air <jchappypig@hotmail.com>
2012-03-11 00:22:22 +1100

    setup default url for action mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ RoyalChinese::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -57,4 +57,6 @@ RoyalChinese::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = {:host => 'royal-chinese.herokuapp.com'}
 end
