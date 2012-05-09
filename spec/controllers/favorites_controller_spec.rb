6035d022a74b16f7988d80d0b9995f59564065f4
Mac book Air <jchappypig@hotmail.com>
2012-05-09 20:30:26 +1000

    Fix test failure.

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -5,7 +5,7 @@ describe FavoritesController do
     let(:menu) {Factory(:menu)}
     it "should save favorite if valid" do
       Favorite.count.should == 0
-      post :create, format: 'json', menu_id: menu
+      post :create, format: 'js', menu_id: menu
       Favorite.count.should == 1
       response.should render_template :create
     end
