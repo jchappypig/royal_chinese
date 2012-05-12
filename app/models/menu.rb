87fcc69b84ec417f54468c6d0e8b2b155ba118b4
Mac book Air <jchappypig@hotmail.com>
2012-05-13 00:45:38 +1000

    added s3 setup for paperclip storage.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -7,7 +7,13 @@ class Menu < ActiveRecord::Base
   has_attached_file :image, styles: { thumbnail: '160x120>',  medium: '260x180>', large: '360x268>' },
                     default_url: '/assets/images/rails.png',
                     url: '/assets/images/menus/:id/:style/:basename.:extension',
-                    path: ':rails_root/public/assets/images/menus/:id/:style/:basename.:extension'
+                    path: ':rails_root/public/assets/images/menus/:id/:style/:basename.:extension',
+                    storage: :s3,
+                    bucket: ENV['S3_BUCKET_NAME'],
+                    s3_credentials: {
+                        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+                    }
   validates_attachment_size :image, less_than: 2.megabytes
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif']
 
