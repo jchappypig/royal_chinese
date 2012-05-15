5736b6175409672a1a42a8dd42504dba4e246352
Mac book Air <jchappypig@hotmail.com>
2012-05-15 18:47:42 +1000

    updated our location css.

--- app/models/address.rb
+++ app/models/address.rb
@@ -11,10 +11,18 @@ class Address < ActiveRecord::Base
   end
 
   def gmaps4rails_infowindow
-    infowindow =  "<h3>#{name}</h3>"
-    infowindow << "<img src='/images/ziqi.jpg' width='100px' align='left'>"
-    infowindow << "<h4>Open Hour:</h4>"
-    infowindow << "<p><dd><b>Monday to Sunday</b><dd></p>"
-    infowindow << "<dd>6:00pm - 10:00pm</dd>"
+    infowindow =  "<h3 class='restaurant_name'>#{name}</h3>"
+    infowindow << "<div class='row' >"
+    infowindow << " <div class='span2 landmark' >"
+    infowindow << "   <img src='/assets/flag.png' width='100px' align='left'/>"
+    infowindow << " </div>"
+    infowindow << " <div class='span2' >"
+    infowindow << "   <strong>Open Hour:</strong>"
+    infowindow << "   <p/>"
+    infowindow << "   <em>Monday to Sunday</em>"
+    infowindow << "   <p>11:00am - 02:00pm</p>"
+    infowindow << "   <p>05:30pm - 10:00pm</p>"
+    infowindow << " </div>"
+    infowindow << "</div>"
   end
 end
