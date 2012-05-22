a076e94fc35ed7f1b0a197101a5320b6ee39090a
Mac book Air <jchappypig@hotmail.com>
2012-05-22 19:29:10 +1000

    added broadcast preview.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,27 @@
 module ApplicationHelper
 
   def company_name
-    "Royal Chinese"
+    'Royal Chinese'
   end
 
   def company_full_name
-    company_name + " Restaurant"
+    company_name + ' Restaurant'
   end
 
   def company_contact_email
-    "royal_chinese@hotmail.com"
+    'royal_chinese@hotmail.com'
+  end
+
+  def company_address
+    '35 Flinders Street, Wollongong, NSW 2500'
+  end
+
+  def company_phone
+    '(02) 4226 9853'
   end
 
   def company_contact_email_with_name
-    "\"#{company_name}\" <#{company_contact_email}>"
+    "\'#{company_name}\' <#{company_contact_email}>"
   end
 
 end
